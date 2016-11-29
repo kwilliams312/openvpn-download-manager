83164d2b01d5979a7d8b1a231c56520751ac1c4c
Ken Williams <kwillia@gmail.com>
2016-11-28 22:20:50 -0800

    First commit, based on haugene/docker-transmission-openvpn

---
+++ transmission/start.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Source our persisted env variables from container startup
+. /etc/transmission/environment-variables.sh
+
+# This script will be called with tun/tap device name as parameter 1, and local IP as parameter 4
+# See https://openvpn.net/index.php/open-source/documentation/manuals/65-openvpn-20x-manpage.html (--up cmd)
+echo "Updating TRANSMISSION_BIND_ADDRESS_IPV4 to the ip of $1 : $4"
+export TRANSMISSION_BIND_ADDRESS_IPV4=$4
+
+echo "Generating transmission settings.json from env variables"
+# Ensure TRANSMISSION_HOME is created
+mkdir -p ${TRANSMISSION_HOME}
+dockerize -template /etc/transmission/settings.tmpl:${TRANSMISSION_HOME}/settings.json /bin/true
+
+if [ ! -e "/dev/random" ]; then
+  # Avoid "Fatal: no entropy gathering module detected" error
+  echo "INFO: /dev/random not found - symlink to /dev/urandom"
+  ln -s /dev/urandom /dev/random
+fi
+
+. /etc/transmission/userSetup.sh
+
+echo "STARTING TRANSMISSION"
+exec sudo -u ${RUN_AS} /usr/bin/transmission-daemon -g ${TRANSMISSION_HOME} --logfile ${TRANSMISSION_HOME}/transmission.log &
+
+if [ "$OPENVPN_PROVIDER" = "PIA" ]
+then
+    echo "STARTING PORT UPDATER"
+    exec /etc/transmission/periodicUpdates.sh $4 &
+else
+    echo "NO PORT UPDATER FOR THIS PROVIDER"
+fi
+
+echo "Transmission startup script complete."
